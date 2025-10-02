5d005819591ac3d108caa18c68795c5864750a97
marvelkn <marvel_kevin12@yahoo.com>
2025-10-02 17:42:32 +0700

    Commit No.4 - Assignment

--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -3,44 +3,36 @@ package com.example.lab_week_06
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.ItemTouchHelper // <-- ADD THIS IMPORT
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_06.model.CatBreed
 import com.example.lab_week_06.model.CatModel
 import com.example.lab_week_06.model.Gender
 
-
 class MainActivity : AppCompatActivity() {
     private val recyclerView: RecyclerView by lazy {
         findViewById(R.id.recycler_view)
     }
     private val catAdapter by lazy {
-//Glide is used here to load the images
-//Here we are passing the onClickListener function to the Adapter
-        CatAdapter(layoutInflater, GlideImageLoader(this), object:
+        CatAdapter(layoutInflater, GlideImageLoader(this), object :
             CatAdapter.OnClickListener {
-            //When this is triggered, the pop up dialog will be shown
             override fun onItemClick(cat: CatModel) = showSelectionDialog(cat)
         })
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//Setup the adapter for the recycler view
+
         recyclerView.adapter = catAdapter
-//Setup the layout manager for the recycler view
-//A layout manager is used to set the structure of the item views
-//For this tutorial, we're using the vertical linear structure
         recyclerView.layoutManager = LinearLayoutManager(this,
             LinearLayoutManager.VERTICAL, false)
 
-        //Instantiate ItemTouchHelper for the swipe to delete callback and
-        //attach it to the recycler view [cite: 607]
-        val itemTouchHelper = ItemTouchHelper(catAdapter.swipeToDeleteCallback) // [cite: 608]
-        itemTouchHelper.attachToRecyclerView(recyclerView) // [cite: 608]
+        val itemTouchHelper = ItemTouchHelper(catAdapter.swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
 
-//Add data to the model list in the adapter
+        // Add data to the model list in the adapter
         catAdapter.setData(
             listOf(
                 CatModel(
@@ -63,6 +55,56 @@ class MainActivity : AppCompatActivity() {
                     "Curious George",
                     "Award winning investigator",
                     "https://cdn2.thecatapi.com/images/bar.jpg"
+                ),
+                // --- ADDED 7 MORE CATS BELOW ---
+                CatModel(
+                    Gender.Male,
+                    CatBreed.ExoticShorthair,
+                    "Leo",
+                    "Loves to nap in sunbeams",
+                    "https://cdn2.thecatapi.com/images/aob.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.AmericanCurl,
+                    "Bella",
+                    "Expert at finding trouble",
+                    "https://cdn2.thecatapi.com/images/d56.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.BalineseJavanese,
+                    "Max",
+                    "A true gentleman",
+                    "https://cdn2.thecatapi.com/images/13j.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.AmericanCurl,
+                    "Lucy",
+                    "Queen of the household",
+                    "https://cdn2.thecatapi.com/images/MTc5OTc2Mw.jpg"
+                ),
+                CatModel(
+                    Gender.Male,
+                    CatBreed.ExoticShorthair,
+                    "Charlie",
+                    "Very playful and energetic",
+                    "https://cdn2.thecatapi.com/images/5i3.jpg"
+                ),
+                CatModel(
+                    Gender.Female,
+                    CatBreed.BalineseJavanese,
+                    "Molly",
+                    "Loves belly rubs",
+                    "https://cdn2.thecatapi.com/images/b6f.jpg"
+                ),
+                CatModel(
+                    Gender.Unknown,
+                    CatBreed.ExoticShorthair,
+                    "Smokey",
+                    "Mysterious and aloof",
+                    "https://cdn2.thecatapi.com/images/c2f.jpg"
                 )
             )
         )
@@ -70,11 +112,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun showSelectionDialog(cat: CatModel) {
         AlertDialog.Builder(this)
-//Set the title for the dialog
             .setTitle("Cat Selected")
-//Set the message for the dialog
             .setMessage("You have selected cat ${cat.name}")
-//Set if the OK button should be enabled
             .setPositiveButton("OK") { _, _ -> }.show()
     }
 }
